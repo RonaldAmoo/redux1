729cc90e47d27c756d165e979d635d6b538ae13d
Ronald Amoo <butsormeamoo@gmail.com>
2021-06-10 11:10:10 +0000

    remove bootstrap import

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
 import React, { Component } from "react";
-import "bootstrap/dist/css/bootstrap.min.css";
 import { connect } from "react-redux";
 import { addUser, deleteUser } from "./Store/usersAction";
 import UsersForm from "./components/UsersForm";
